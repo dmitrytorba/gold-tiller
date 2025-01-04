7594d4a8331179c0711bfb7ecf60c8337d3ffbcd
Dmitry Torba <dmitrytorba@gmail.com>
2025-01-04 08:50:33 -0800

    feat(parser) add spaces

--- pdf-parse.js
+++ pdf-parse.js
@@ -1,29 +1,53 @@
 const pdf = require('pdf-parse');
 const fs = require('fs');
 
-const dataBuffer = fs.readFileSync('./test.pdf');
+function render_page(pageData) {
+    return pageData.getTextContent()
+	.then(function(textContent) {
+		let lastY, text = '';
+		for (let item of textContent.items) {
+			if (lastY == item.transform[5] || !lastY){
+				text += ' ' + item.str;
+			}  
+			else{
+				text += '\n' + item.str;
+			}    
+			lastY = item.transform[5];
+		}
+		return text;
+	});
+}
 
+let options = {
+    pagerender: render_page
+}
+
+
+const dataBuffer = fs.readFileSync('./test.pdf');
 let csvOutput = 'Date,Description,Amount\n';
  
-pdf(dataBuffer).then(function(data) {
-    const tables = data.text.split(`Tran\nDate\nPost\nDateReference NumberTransaction DescriptionAmount`);
+pdf(dataBuffer,options).then(function(data) {
+    // console.log(data.text);
+    const tables = data.text.split(`Tran\nDate\nPost\nDate Reference Number Transaction Description Amount`);
     for (const table of tables) {
         console.log('-------------------');
         const lines = table.split('\n');
         for (const line of lines) {
             if (line.match(/^\d*\//)) {
-                console.log(line);
+                // console.log(line);
                 const date = line.slice(0, 5);
-                const prefix = line.split(' ')[0];
-                const priceMatches = line.match(/[+-]?([0-9]*[.])?[0-9]+$/);
-                if (priceMatches) {
-                    const price = priceMatches[0];
-                    const description = line.replace(price, '').replace(prefix, '').trim();
-                    console.log('amount:', price);
-                    console.log('date:', date);
-                    console.log('description:', description);
-                    csvOutput += `${date},\"${description}\",${price}\n`;
+                const sections = line.split(' ');
+                let total = sections[sections.length - 1];
+                if (total[total.length - 1] === '-') {
+                    total = total.slice(0, total.length - 1);
+                } else {
+                    total = `-${total}`;
                 }
+                const description = sections.slice(3, sections.length - 1).join(' ');
+                // console.log('total:', total);
+                // console.log('date:', date);
+                // console.log('description:', description);
+                csvOutput += `${date},\"${description}\",${total}\n`;
             }
             if (line.match("Transactions continued on next page")) {
                 break;
