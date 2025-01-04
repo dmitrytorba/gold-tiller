0668f3af4a236135900be9df9441b870397d67f2
Dmitry Torba <dmitrytorba@gmail.com>
2025-01-03 19:59:31 -0800

    feat(parser) write csv

--- pdf-parse.js
+++ pdf-parse.js
@@ -1,7 +1,9 @@
 const pdf = require('pdf-parse');
 const fs = require('fs');
 
-let dataBuffer = fs.readFileSync('./test.pdf');
+const dataBuffer = fs.readFileSync('./test.pdf');
+
+let csvOutput = 'Date,Description,Amount\n';
  
 pdf(dataBuffer).then(function(data) {
     const tables = data.text.split(`Tran\nDate\nPost\nDateReference NumberTransaction DescriptionAmount`);
@@ -20,13 +22,19 @@ pdf(dataBuffer).then(function(data) {
                     console.log('amount:', price);
                     console.log('date:', date);
                     console.log('description:', description);
+                    csvOutput += `${date},\"${description}\",${price}\n`;
                 }
             }
             if (line.match("Transactions continued on next page")) {
                 break;
             }
         }
-    }
-    
-        
+    }    
+    fs.writeFile('output.csv', csvOutput, 'utf8', function (err) {
+        if (err) {
+        console.log('Some error occured - file either not saved or corrupted file saved.');
+        } else{
+        console.log('It\'s saved!');
+        }
+    });
 });
