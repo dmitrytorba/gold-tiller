2176da1c220914f81881e755e427ba68b0d75274
Dmitry Torba <dmitrytorba@gmail.com>
2025-01-03 19:17:55 -0800

    feat(parser) get txn columns

--- pdf-parse.js
+++ pdf-parse.js
@@ -1,20 +1,26 @@
 const pdf = require('pdf-parse');
 const fs = require('fs');
 
-// console.log('haha')
 let dataBuffer = fs.readFileSync('./test.pdf');
  
 pdf(dataBuffer).then(function(data) {
-    // console.log(data.text);
     const tables = data.text.split(`Tran\nDate\nPost\nDateReference NumberTransaction DescriptionAmount`);
     for (const table of tables) {
         console.log('-------------------');
-        // console.log(table);
         const lines = table.split('\n');
         for (const line of lines) {
-            // console.log(line);
             if (line.match(/^\d*\//)) {
                 console.log(line);
+                const date = line.slice(0, 5);
+                const prefix = line.split(' ')[0];
+                const priceMatches = line.match(/[+-]?([0-9]*[.])?[0-9]+$/);
+                if (priceMatches) {
+                    const price = priceMatches[0];
+                    const description = line.replace(price, '').replace(prefix, '').trim();
+                    console.log('amount:', price);
+                    console.log('date:', date);
+                    console.log('description:', description);
+                }
             }
             if (line.match("Transactions continued on next page")) {
                 break;
